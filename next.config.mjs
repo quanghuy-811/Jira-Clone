d3083bcc68ddeb565082bbd025eee9e4f2f4f07d
quanghuy-811 <anhthu157.do@gmail.com>
2025-03-19 21:35:15 +0700

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    appDir: true, // Đảm bảo sử dụng App Router
+  },
+  output: "standalone", // Giúp deploy dễ dàng hơn trên Vercel
+  revalidate: 0, // 🔥 Ngăn Next.js cache trang để tránh lỗi SSG
+};
 
 export default nextConfig;
