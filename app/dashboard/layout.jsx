db9fe99af90e81c444880029b26ef7d69d6801d1
quanghuy-811 <anhthu157.do@gmail.com>
2025-04-19 10:07:27 +0700

    update

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -7,6 +7,9 @@ export default function DashboardLayout({ children }) {
       <MainNav />
       <main className="container flex-grow mx-auto py-6 px-4">{children}</main>
       {/* <Footer /> */}
+      <footer className="w-full text-center py-4 text-sm text-gray-500 border-t">
+        Copyright © 2025 Jira
+      </footer>
     </div>
   );
 }
